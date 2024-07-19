212698ce9ba620de20f61865253bf70ca1d59f86
Cristian Sandoval <cristiansandoval2202@gmail.com>
2024-07-18 21:56:41 -0500

    clone netflix

---
+++ assets/script/app.js
@@ -0,0 +1,52 @@
+const item1 = document.querySelector(".btn__faq-1");
+const itemsDescription1 = document.querySelector(".faq__content-1")
+
+
+item1.addEventListener("click", () =>{
+    itemsDescription1.classList.toggle("show__faq")
+});
+
+
+const item2 = document.querySelector(".btn__faq-2");
+const itemsDescription2 = document.querySelector(".faq__content-2")
+
+
+item2.addEventListener("click", () =>{
+     itemsDescription2.classList.toggle("show__faq")
+});
+
+const item3 = document.querySelector(".btn__faq-3");
+const itemsDescription3 = document.querySelector(".faq__content-3")
+
+
+item3.addEventListener("click", () =>{
+     itemsDescription3.classList.toggle("show__faq")
+});
+
+const item4 = document.querySelector(".btn__faq-4");
+const itemsDescription4 = document.querySelector(".faq__content-4")
+
+
+item4.addEventListener("click", () =>{
+     itemsDescription4.classList.toggle("show__faq")
+});
+
+const item5 = document.querySelector(".btn__faq-5");
+const itemsDescription5 = document.querySelector(".faq__content-5")
+
+
+item5.addEventListener("click", () =>{
+     itemsDescription5.classList.toggle("show__faq")
+});
+
+const item6 = document.querySelector(".btn__faq-6");
+const itemsDescription6 = document.querySelector(".faq__content-6")
+
+
+item6.addEventListener("click", () =>{
+     itemsDescription6.classList.toggle("show__faq")
+});
+
+
+
+
